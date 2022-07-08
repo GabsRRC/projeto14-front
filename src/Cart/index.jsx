761067a3fe4f9fc3499594bc11c1fe19400c75a8
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-07-08 14:18:57 -0300

    wip: cart front

--- src/Cart/index.jsx
+++ src/Cart/index.jsx
@@ -1,10 +1,49 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Cart() {
   return (
-    <>
-      <div>Carrinho</div>
-      <Link to="/checkout">Prossiga para o pagamento!</Link>
-    </>
+    <CartStyled>
+      <CartProducts>
+        <h1> Produtos do carrinho</h1>
+        <Products>
+          <ul>
+            <li>FUnko 1</li>
+            <li>FUnko 1</li>
+            <li>FUnko 1</li>
+            <li>FUnko 1</li>
+            <li>FUnko 1</li>
+            <li>FUnko 1</li>
+          </ul>
+        </Products>
+      </CartProducts>
+      <div>
+        {" "}
+        <div>Pagamento</div>
+        <Link to="/checkout">Prossiga para o pagamento!</Link>
+      </div>
+    </CartStyled>
   );
 }
+
+const CartStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  h1 {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+`;
+const CartProducts = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 50px;
+`;
+const Products = styled.div``;
