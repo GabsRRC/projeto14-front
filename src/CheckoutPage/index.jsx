9514b2a8bc84efa0de6fa3233281ba0cfeb41095
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-07-11 12:03:24 -0300

    checkout finalizado com post para api e registro no banco de dados

--- src/CheckoutPage/index.jsx
+++ src/CheckoutPage/index.jsx
@@ -1,69 +1,107 @@
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import CartContext from "../contexts/CartContext.js";
 import pix from "../assets/pix-106.svg";
 import creditCard from "../assets/credit-card.svg";
 import boleto from "../assets/boleto.svg";
+import CartContext from "../contexts/CartContext.js";
 import TokenContext from "../contexts/TokenContext.js";
+import UserContext from "../contexts/UserContext.js";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Checkout() {
+  const navigate = useNavigate();
   const { cart, setCart } = useContext(CartContext);
-  const [payment, setPaymant] = useState("");
+  const [payment, setPayment] = useState("");
   const [alertText, setAlertText] = useState("");
-  const {token} = useContext(TokenContext)
+  const { token } = useContext(TokenContext);
+  const { userInfo } = useContext(UserContext);
+  const [userLogged, setUserLogged] = useState(false);
 
-  function choosePaymentMethod(e) {}
-  function payFinish() {
-    if (payment !== "") {
-      console.log("Pagou");
-      console.log(payment);
-      console.log(cart);
-      console.log(token);
+  useEffect(() => {
+    if (token !== "") {
+      setUserLogged(true);
+    }
+  }, []);
 
-      setAlertText("Escolha um método de pagamento para prosseguir!")
+  function payFinish(e) {
+    if (payment === "") {
+      setAlertText("Escolha um método de pagamento para prosseguir!");
+    } else {
+      if (token !== "" && cart.length !== 0 && userLogged === true) {
+        e.preventDefault();
+        setAlertText("");
+        const auth = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const userCheckout = {
+          name: userInfo.name,
+          buyedProducts: cart,
+          paymentMethod: payment,
+        };
+
+        axios.post("http://localhost:5000/checkout", userCheckout, auth);
+        setCart([]);
+        navigate("/");
+      } else {
+        setAlertText("Por favor, faça o login, ou adicione itens ao carrinho!");
+      }
     }
   }
+
   return (
     <CheckoutStyled>
       <h1>Escolha sua forma de pagamento!</h1>
-      <CheckoutMetods>
-        <label>
-          <input
-            type="radio"
-            className="pagamento"
-            value="pix"
-            name="pagamento"
-            onChange={(e) => setPaymant(e.target.value)}
-          />
-          <img src={pix} />
-          Pix
-        </label>
-        <label>
-          <input
-            type="radio"
-            className="pagamento"
-            value="boleto"
-            name="pagamento"
-            onChange={(e) => setPaymant(e.target.value)}
-          />
-          <img src={boleto} />
-          Boleto Bancário
-        </label>
-        <label>
-          <input
-            type="radio"
-            className="pagamento"
-            value="cartao-de-credito"
-            name="pagamento"
-            onChange={(e) => setPaymant(e.target.value)}
-          />
-          <img src={creditCard} />
-          Cartão de crédito
-        </label>
-      </CheckoutMetods>
+      {userLogged ? (
+        <>
+          <CheckoutMetods>
+            <label>
+              <input
+                type="radio"
+                className="pagamento"
+                value="pix"
+                name="pagamento"
+                onChange={(e) => setPayment(e.target.value)}
+              />
+              <img src={pix} />
+              Pix
+            </label>
+            <label>
+              <input
+                type="radio"
+                className="pagamento"
+                value="boleto"
+                name="pagamento"
+                onChange={(e) => setPayment(e.target.value)}
+              />
+              <img src={boleto} />
+              Boleto Bancário
+            </label>
+            <label>
+              <input
+                type="radio"
+                className="pagamento"
+                value="cartao-de-credito"
+                name="pagamento"
+                onChange={(e) => setPayment(e.target.value)}
+              />
+              <img src={creditCard} />
+              Cartão de crédito
+            </label>
+          </CheckoutMetods>
+          <button onClick={payFinish}>Finalizar o pagamento!</button>
+        </>
+      ) : (
+        <UserOff>
+          Por favor, faça o login para poder finalizar o pagamento!
+        </UserOff>
+      )}
 
-      <button onClick={payFinish}>Finalizar o pagamento!</button>
       <p>{alertText}</p>
     </CheckoutStyled>
   );
@@ -78,14 +116,15 @@ const CheckoutStyled = styled.div`
   font-family: "roboto";
   text-align: center;
 
-p{
-  color: red;
-  margin-top: 50px;
-  font-weight: 700;
-  font-size: 22px;
-}
+  p {
+    color: red;
+    margin-top: 50px;
+    font-weight: 700;
+    font-size: 22px;
+  }
 
   h1 {
+    margin-top: 20px;
     font-size: 24px;
     font-weight: bold;
     margin-bottom: 30px;
@@ -110,7 +149,7 @@ p{
     font-weight: 700;
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
       rgba(0, 0, 0, 0.23) 0px 6px 6px;
-    border:0;
+    border: 0;
 
     :hover {
       background-color: darkgreen;
@@ -150,4 +189,9 @@ const CheckoutMetods = styled.div`
       margin-right: 20px;
     }
   }
+`;
+const UserOff = styled.div`
+  width: 90%;
+  font-size: 22px;
+  font-weight: bold;
 `;
